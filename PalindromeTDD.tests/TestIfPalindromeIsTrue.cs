8f5071d9732d2db4b7aa7c8ea9f8c9bd6eca8bf0
Chris LoJacono <clojacono92@gmail.com>
2021-04-08 13:00:58 -0500

    saved

---
+++ PalindromeTDD.tests/TestIfPalindromeIsTrue.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+
+namespace PalindromeTDD.tests
+{
+    public class TestIfPalindromeIsTrue
+    {
+        [Fact]
+        public void This_will_pass_if_stars_is_false()
+        {
+            //Assert
+            var testString = "stars";
+            var expectedResult = false;
+
+            var tester = new PalindromeTester();
+            var testResult = tester.Tester(testString);
+
+            Assert.Equal(expectedResult, testResult);
+        }
+    }
+}
---
+++ PalindromeTDD/PalindromeTester.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace PalindromeTDD
+{
+    public class PalindromeTester
+    {
+        public bool Tester(string input)
+        {
+            if (input == input.Reverse())
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
