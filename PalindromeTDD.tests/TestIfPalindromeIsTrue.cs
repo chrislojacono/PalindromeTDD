34ab7f7cb1a044b2e978da17ee22cd0a5e1953cb
Chris LoJacono <clojacono92@gmail.com>
2021-04-08 17:34:40 -0500

    test 2 written

--- PalindromeTDD.tests/TestIfPalindromeIsTrue.cs
+++ PalindromeTDD.tests/TestIfPalindromeIsTrue.cs
@@ -17,5 +17,17 @@ namespace PalindromeTDD.tests
 
             Assert.Equal(expectedResult, testResult);
         }
+
+        [Fact]
+        public void This_will_pass_if_Uwakov_is_true()
+        {
+            var testString = "O, a kak Uwakov lil vo kawu kakao!";
+            var expectedResult = true;
+            var tester = new PalindromeTester();
+
+            var testResult = tester.Tester(testString);
+
+            Assert.Equal(expectedResult, testResult);
+        }
     }
 }
